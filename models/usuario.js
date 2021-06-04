8cb2e5e66e19ae41ba404fd283b7c412db21fa82
Jhonatan <jhonatan.espinoza3194@gmail.com>
2021-06-03 21:44:28 -0500

    .env omitido

--- models/usuario.js
+++ models/usuario.js
@@ -3,8 +3,39 @@ const{Schema,model}= require('mongoose');
 
 const UsuarioSchema=Schema({
 
-    
+    nombre:{
+        type:String,
+        required:[true,'Nombre obligatorio']
+    },
+    correo:{
+        type:String,
+        required:[true,'Correo obligatorio'],
+        unique:true
+    },
+    contrasena:{
+        type:String,
+        required:[true,'Contraseña obligatorio']
+    },
+    img:{
+        type:String
+       
+    },
+    rol:{
+        type:String,
+        required:true,
+        enum:['ROL ADMINISTRADOR','ROL USUARIO']
+    },
+    estado:{
+        type:Boolean,
+       default:true
+    },
+    google:{
+        type:Boolean,
+       default:false
+    }
+
+
 });
 
 
-module.exports={};
+module.exports=model('Usuario',UsuarioSchema);
